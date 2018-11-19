57baf4eb482f6301c5d39a6f9e4a177ce85a4dca
zenazor13 <44451325+zenazor13@users.noreply.github.com>
2018-11-19 12:23:04 -0500

    Changed questions to only use words of same type

--- src/FlashCardsGUI.java
+++ src/FlashCardsGUI.java
@@ -25,8 +25,13 @@ public class FlashCardsGUI {
 	private JButton settings;
 
 	private Vector<Word> wordlist = new Vector<>();
+	private Vector<Word> preplist = new Vector<>();
+	private Vector<Word> verblist = new Vector<>();
+	private Vector<Word> nounlist = new Vector<>();
 	private Vector<Word> buttonText = new Vector<>(); //keeps track of what words are used on which button
 	private Word correctWord = new Word();
+	private Word rand1 = new Word();
+	private Word rand2 = new Word();
 	private String questionType;
 	private HashMap<Word, Integer> wordmap = new HashMap<>();
 
@@ -60,9 +65,7 @@ public class FlashCardsGUI {
 
 		buttonText.clear();
 
-		// pick word on list to be the answer
-		int randWord = rand.nextInt(wordlist.size());
-		correctWord = wordlist.elementAt(randWord);
+		getRandomWords();
 
 		// pick scenario
 		getQuestionType();
@@ -70,11 +73,11 @@ public class FlashCardsGUI {
 		// choose between various output scenarios
 		switch (questionType) {
 			case "pickArticle": {
-				if (wordlist.elementAt(randWord) instanceof Noun) {
-					if (((Noun) wordlist.elementAt(randWord)).getArticle().equalsIgnoreCase(" ")) {
+				if (correctWord instanceof Noun) {
+					if (((Noun) correctWord).getArticle().equalsIgnoreCase(" ")) {
 						resetButtons();
 					}
-					pickArticle(randWord);
+					pickArticle();
 				}
 				else {
 					resetButtons();
@@ -82,11 +85,11 @@ public class FlashCardsGUI {
 				break;
 			}
 			case "pickDefinition": {
-				pickDefinition(randWord);
+				pickDefinition();
 				break;
 			}
 			case "pickWord": {
-				pickWord(randWord);
+				pickWord();
 				break;
 			}
 		}
@@ -109,86 +112,110 @@ public class FlashCardsGUI {
 		}
 	}
 
-	private void pickArticle(int randWord) {
+	private void getRandomWords() {
 
-		button0.setText("der " + wordlist.elementAt(randWord).getWord());
-		buttonText.add(wordlist.elementAt(randWord));
-		button1.setText("die " + wordlist.elementAt(randWord).getWord());
-		buttonText.add(wordlist.elementAt(randWord));
-		button2.setText("das " + wordlist.elementAt(randWord).getWord());
-		buttonText.add(wordlist.elementAt(randWord));
+		correctWord = wordlist.elementAt(rand.nextInt(wordlist.size()));
 
-		labelQuestion.setText(wordlist.elementAt(randWord).getDefinition());
+		if (correctWord instanceof Prep) {
+			rand1 = preplist.elementAt(rand.nextInt(preplist.size()));
+			rand2 = preplist.elementAt(rand.nextInt(preplist.size()));
+
+			while (rand1.equals(correctWord) || rand2.equals(correctWord) || rand1.equals(rand2)) {
+				rand1 = preplist.elementAt(rand.nextInt(preplist.size()));
+				rand2 = preplist.elementAt(rand.nextInt(preplist.size()));
+			}
+		}
+		else if (correctWord instanceof Verb) {
+			rand1 = verblist.elementAt(rand.nextInt(verblist.size()));
+			rand2 = verblist.elementAt(rand.nextInt(verblist.size()));
+
+			while (rand1.equals(correctWord) || rand2.equals(correctWord) || rand1 == rand2) {
+				rand1 = verblist.elementAt(rand.nextInt(verblist.size()));
+				rand2 = verblist.elementAt(rand.nextInt(verblist.size()));
+			}
+		}
+		else if (correctWord instanceof Noun) {
+			rand1 = nounlist.elementAt(rand.nextInt(nounlist.size()));
+			rand2 = nounlist.elementAt(rand.nextInt(nounlist.size()));
+
+			while (rand1.equals(correctWord) || rand2.equals(correctWord) || rand1 == rand2) {
+				rand1 = nounlist.elementAt(rand.nextInt(nounlist.size()));
+				rand2 = nounlist.elementAt(rand.nextInt(nounlist.size()));
+			}
+		}
+
+	}
+
+	private void pickArticle() {
+
+		button0.setText("der " + correctWord.getWord());
+		buttonText.add(correctWord);
+		button1.setText("die " + correctWord.getWord());
+		buttonText.add(correctWord);
+		button2.setText("das " + correctWord.getWord());
+		buttonText.add(correctWord);
+
+		labelQuestion.setText(correctWord.getDefinition());
 
 		timesCorrect.setText("Times correct: " + wordmap.get(correctWord));
 		wordsLeft.setText("Words left: " + (wordlist.size() - 2));
 
 	}
 
-	private void pickDefinition(int randWord) {
-
-		// pick random definitions to also be displayed
-		int rand1 = rand.nextInt(wordlist.size());
-		int rand2 = rand.nextInt(wordlist.size());
-
-
-		while (rand1 == randWord || rand2 == randWord || rand1 == rand2) {
-			rand1 = rand.nextInt(wordlist.size());
-			rand2 = rand.nextInt(wordlist.size());
-		}
+	private void pickDefinition() {
 
 		// pick a random button to be correct
 		switch (rand.nextInt(3)) {
 			case 0: {
-				button0.setText(wordlist.elementAt(randWord).getDefinition());
-				buttonText.add(wordlist.elementAt(randWord));
-				button1.setText(wordlist.elementAt(rand1).getDefinition());
-				buttonText.add(wordlist.elementAt(rand1));
-				button2.setText(wordlist.elementAt(rand2).getDefinition());
-				buttonText.add(wordlist.elementAt(rand2));
+				button0.setText(correctWord.getDefinition());
+				buttonText.add(correctWord);
+				button1.setText(rand1.getDefinition());
+				buttonText.add(rand1);
+				button2.setText(rand2.getDefinition());
+				buttonText.add(rand2);
 
-				if (wordlist.elementAt(randWord) instanceof Noun) {
-					labelQuestion.setText(((Noun) wordlist.elementAt(randWord)).getArticle() + " " +
-							wordlist.elementAt(randWord).getWord());
+				if (correctWord instanceof Noun) {
+					labelQuestion.setText(((Noun) correctWord).getArticle() + " " +
+							correctWord.getWord());
 				}
 				else {
-					labelQuestion.setText(wordlist.elementAt(randWord).getWord());
+					labelQuestion.setText(correctWord.getWord());
 				}
 
 				break;
 			}
 			case 1: {
-				button0.setText(wordlist.elementAt(rand1).getDefinition());
-				buttonText.add(wordlist.elementAt(rand1));
-				button1.setText(wordlist.elementAt(randWord).getDefinition());
-				buttonText.add(wordlist.elementAt(randWord));
-				button2.setText(wordlist.elementAt(rand2).getDefinition());
-				buttonText.add(wordlist.elementAt(rand2));
+				button0.setText(rand1.getDefinition());
+				buttonText.add(rand1);
+				button1.setText(correctWord.getDefinition());
+				buttonText.add(correctWord);
+				button2.setText(rand2.getDefinition());
+				buttonText.add(rand2);
 
-				if (wordlist.elementAt(randWord) instanceof Noun) {
-					labelQuestion.setText(((Noun) wordlist.elementAt(randWord)).getArticle() + " " +
-							wordlist.elementAt(randWord).getWord());
+				if (correctWord instanceof Noun) {
+					labelQuestion.setText(((Noun) correctWord).getArticle() + " " +
+							correctWord.getWord());
 				}
 				else {
-					labelQuestion.setText(wordlist.elementAt(randWord).getWord());
+					labelQuestion.setText(correctWord.getWord());
 				}
 
 				break;
 			}
 			case 2: {
-				button0.setText(wordlist.elementAt(rand1).getDefinition());
-				buttonText.add(wordlist.elementAt(rand1));
-				button1.setText(wordlist.elementAt(rand2).getDefinition());
-				buttonText.add(wordlist.elementAt(rand2));
-				button2.setText(wordlist.elementAt(randWord).getDefinition());
-				buttonText.add(wordlist.elementAt(randWord));
+				button0.setText(rand1.getDefinition());
+				buttonText.add(rand1);
+				button1.setText(rand2.getDefinition());
+				buttonText.add(rand2);
+				button2.setText(correctWord.getDefinition());
+				buttonText.add(correctWord);
 
-				if (wordlist.elementAt(randWord) instanceof Noun) {
-					labelQuestion.setText(((Noun) wordlist.elementAt(randWord)).getArticle() + " " +
-							wordlist.elementAt(randWord).getWord());
+				if (correctWord instanceof Noun) {
+					labelQuestion.setText(((Noun) correctWord).getArticle() + " " +
+							correctWord.getWord());
 				}
 				else {
-					labelQuestion.setText(wordlist.elementAt(randWord).getWord());
+					labelQuestion.setText(correctWord.getWord());
 				}
 
 				break;
@@ -203,117 +230,107 @@ public class FlashCardsGUI {
 
 	}
 
-	private void pickWord(int randWord) {
-
-
-		// pick random words to be displayed
-		int rand1 = rand.nextInt(wordlist.size());
-		int rand2 = rand.nextInt(wordlist.size());
-
-		while (rand1 == randWord || rand2 == randWord || rand1 == rand2) {
-			rand1 = rand.nextInt(wordlist.size());
-			rand2 = rand.nextInt(wordlist.size());
-		}
+	private void pickWord() {
 
 		switch (rand.nextInt(3)) {
 			case 0: {
-				if (wordlist.elementAt(randWord) instanceof Noun) {
-					button0.setText(((Noun) wordlist.elementAt(randWord)).getArticle()
-							+ " " + wordlist.elementAt(randWord).getWord());
-					buttonText.add(wordlist.elementAt(randWord));
+				if (correctWord instanceof Noun) {
+					button0.setText(((Noun) correctWord).getArticle()
+							+ " " + correctWord.getWord());
+					buttonText.add(correctWord);
 				}
 				else {
-					button0.setText(wordlist.elementAt(randWord).getWord());
-					buttonText.add(wordlist.elementAt(randWord));
+					button0.setText(correctWord.getWord());
+					buttonText.add(correctWord);
 				}
 
-				if (wordlist.elementAt(rand1) instanceof Noun) {
-					button1.setText(((Noun) wordlist.elementAt(rand1)).getArticle()
-							+ " " + wordlist.elementAt(rand1).getWord());
-					buttonText.add(wordlist.elementAt(rand1));
+				if (rand1 instanceof Noun) {
+					button1.setText(((Noun) rand1).getArticle()
+							+ " " + rand1.getWord());
+					buttonText.add(rand1);
 				}
 				else {
-					button1.setText(wordlist.elementAt(rand1).getWord());
-					buttonText.add(wordlist.elementAt(rand1));
+					button1.setText(rand1.getWord());
+					buttonText.add(rand1);
 				}
 
-				if (wordlist.elementAt(rand2) instanceof Noun) {
-					button2.setText(((Noun) wordlist.elementAt(rand2)).getArticle()
-							+ " " + wordlist.elementAt(rand2).getWord());
+				if (rand2 instanceof Noun) {
+					button2.setText(((Noun) rand2).getArticle()
+							+ " " + rand2.getWord());
 				}
 				else {
-					button2.setText(wordlist.elementAt(rand2).getWord());
-					buttonText.add(wordlist.elementAt(rand2));
+					button2.setText(rand2.getWord());
+					buttonText.add(rand2);
 				}
-				labelQuestion.setText(wordlist.elementAt(randWord).getDefinition());
+				labelQuestion.setText(correctWord.getDefinition());
 				break;
 			}
 			case 1: {
-				if (wordlist.elementAt(rand1) instanceof Noun) {
-					button0.setText(((Noun) wordlist.elementAt(rand1)).getArticle()
-							+ " " + wordlist.elementAt(rand1).getWord());
-					buttonText.add(wordlist.elementAt(rand1));
+				if (rand1 instanceof Noun) {
+					button0.setText(((Noun) rand1).getArticle()
+							+ " " + rand1.getWord());
+					buttonText.add(rand1);
 				}
 				else {
-					button0.setText(wordlist.elementAt(rand1).getWord());
-					buttonText.add(wordlist.elementAt(rand1));
+					button0.setText(rand1.getWord());
+					buttonText.add(rand1);
 				}
 
-				if (wordlist.elementAt(randWord) instanceof Noun) {
-					button1.setText(((Noun) wordlist.elementAt(randWord)).getArticle()
-							+ " " + wordlist.elementAt(randWord).getWord());
-					buttonText.add(wordlist.elementAt(randWord));
+				if (correctWord instanceof Noun) {
+					button1.setText(((Noun) correctWord).getArticle()
+							+ " " + correctWord.getWord());
+					buttonText.add(correctWord);
 				}
 				else {
-					button1.setText(wordlist.elementAt(randWord).getWord());
-					buttonText.add(wordlist.elementAt(randWord));
+					button1.setText(correctWord.getWord());
+					buttonText.add(correctWord);
 				}
 
-				if (wordlist.elementAt(rand2) instanceof Noun) {
-					button2.setText(((Noun) wordlist.elementAt(rand2)).getArticle()
-							+ " " + wordlist.elementAt(rand2).getWord());
-					buttonText.add(wordlist.elementAt(rand2));
+				if (rand2 instanceof Noun) {
+					button2.setText(((Noun) rand2).getArticle()
+							+ " " + rand2.getWord());
+					buttonText.add(rand2);
 				}
 				else {
-					button2.setText(wordlist.elementAt(rand2).getWord());
-					buttonText.add(wordlist.elementAt(rand2));
+					button2.setText(rand2.getWord());
+					buttonText.add(rand2);
 				}
 
-				labelQuestion.setText(wordlist.elementAt(randWord).getDefinition());
+				labelQuestion.setText(correctWord.getDefinition());
 				break;
 			}
 			case 2: {
-				if (wordlist.elementAt(rand1) instanceof Noun) {
-					button0.setText(((Noun) wordlist.elementAt(rand1)).getArticle()
-							+ " " + wordlist.elementAt(rand1).getWord());
-					buttonText.add(wordlist.elementAt(rand1));
+				if (rand1 instanceof Noun) {
+					button0.setText(((Noun) rand1).getArticle()
+							+ " " + rand1.getWord());
+					buttonText.add(rand1);
 				}
 				else {
-					button0.setText(wordlist.elementAt(rand1).getWord());
-					buttonText.add(wordlist.elementAt(rand1));
+					button0.setText(rand1.getWord());
+					buttonText.add(rand1);
 				}
 
-				if (wordlist.elementAt(rand2) instanceof Noun) {
-					button1.setText(((Noun) wordlist.elementAt(rand2)).getArticle()
-							+ " " + wordlist.elementAt(rand2).getWord());
-					buttonText.add(wordlist.elementAt(rand2));
+				if (rand2 instanceof Noun) {
+					button1.setText(((Noun) rand2).getArticle()
+							+ " " + rand2.getWord());
+					buttonText.add(rand2);
 				}
 				else {
-					button1.setText(wordlist.elementAt(rand2).getWord());
-					buttonText.add(wordlist.elementAt(rand2));
+					button1.setText(rand2.getWord());
+					buttonText.add(rand2);
 				}
 
-				if (wordlist.elementAt(randWord) instanceof Noun) {
-					button2.setText(((Noun) wordlist.elementAt(randWord)).getArticle()
-							+ " " + wordlist.elementAt(randWord).getWord());
-					buttonText.add(wordlist.elementAt(randWord));
+				if (correctWord instanceof Noun) {
+					button2.setText(((Noun) correctWord).getArticle()
+							+ " " + correctWord.getWord());
+					buttonText.add(correctWord);
 				}
 				else {
-					button2.setText(wordlist.elementAt(randWord).getWord());
-					buttonText.add(wordlist.elementAt(randWord));
+					button2.setText(correctWord.getWord());
+					buttonText.add(correctWord);
 				}
 
-				labelQuestion.setText(wordlist.elementAt(randWord).getDefinition());
+				labelQuestion.setText(correctWord.getDefinition());
 				break;
 			}
 		}
@@ -361,6 +378,7 @@ public class FlashCardsGUI {
 
 				Prep p = new Prep(words[0], words[1]);
 				wordlist.addElement(p);
+				preplist.addElement(p);
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -377,6 +395,7 @@ public class FlashCardsGUI {
 
 				Verb v = new Verb(words[0], words[1]);
 				wordlist.addElement(v);
+				verblist.addElement(v);
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -393,6 +412,7 @@ public class FlashCardsGUI {
 
 				Noun n = new Noun(words[0], words[1], words[2]);
 				wordlist.addElement(n);
+				nounlist.addElement(n);
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
